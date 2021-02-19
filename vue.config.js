951838ef57961a96b2c8a4b7ff4eadc4b2843e9a
Gerhard Jonsson <3613777+Nerderer@users.noreply.github.com>
2021-02-20 00:25:29 +0100

    Fixed generated path to css/js in the demo output.

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   filenameHashing: false,
   outputDir: 'demo',
+  publicPath: '',
   configureWebpack: {
     entry: {
       app: path.join(__dirname, 'demo-src', 'main.js')
